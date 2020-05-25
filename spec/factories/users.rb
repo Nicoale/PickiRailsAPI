c0ca603d2b8376a8ce309f472e7a1f2bd223e097
fadhili-ed <fadhilied00@gmail.com>
2020-05-25 15:37:38 +0300

    ensure user has a unique email address, validate presence of password

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    name { Faker::Internet.name }
+    email { Faker::Internet.safe_email }
+    password_digest { Faker::Internet.password }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,12 +12,13 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:name)}
     it { is_expected.to validate_presence_of(:email)}
     it 'expects email to be valid' do
-      email = User.new(name: 'jon doe', email: 'jondoe@gmail.com')
-      expect(email).to be_valid
+      new_user = User.new(name: 'jon doe', email: 'jondoe@gmail.com', password_digest: '123Jondo#')
+      expect(new_user).to be_valid
     end
     it 'does not allow invalid email' do
       invalid_email = User.new(name: 'jon doe', email: 'jondoe.com')
       expect(invalid_email).to_not be_valid
     end
+    it { is_expected.to validate_presence_of(:password_digest)}
   end
 end
