01e20e46fcf561682313336223ef6fe7f4401733
fadhili-ed <fadhilied00@gmail.com>
2020-05-25 16:10:57 +0300

    correct syntax errors, add validation for uniqueness of email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,16 +9,23 @@ RSpec.describe User, type: :model do
   end
 
   describe 'valid attributes' do
-    it { is_expected.to validate_presence_of(:name)}
-    it { is_expected.to validate_presence_of(:email)}
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+    
     it 'expects email to be valid' do
-      new_user = User.new(name: 'jon doe', email: 'jondoe@gmail.com', password_digest: '123Jondo#')
+      new_user = User.new(name: 'jon doe', email: 'jondoe@gmail.com',
+               password_digest: '123Jondo#')
       expect(new_user).to be_valid
     end
+
     it 'does not allow invalid email' do
       invalid_email = User.new(name: 'jon doe', email: 'jondoe.com')
       expect(invalid_email).to_not be_valid
     end
-    it { is_expected.to validate_presence_of(:password_digest)}
+
+    subject { User.create(name: 'jon doe', email: 'jondoe@gmail.com',
+             password_digest: '123Jondo#') }
+    it { should validate_uniqueness_of(:email) }
+    it { is_expected.to validate_presence_of(:password_digest) }
   end
 end
