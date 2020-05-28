8173143bf718e97244361ddf35a780deb261cabe
nicoale <nicoale8786@gmail.com>
2020-05-28 14:32:59 +0300

    finds existing rider based on the email params,authenticates using the password, assign rider id as the payload, creates a token based session using the payload and jwt session, sets a cookie with the JWTtoken renders the json and csrf to avoid COR vulnerabilites otherwise declares unauthorised and renders an error,also  avails logout option, creates a session using the payload

--- app/controllers/api/v1/rider_logins_controller.rb
+++ app/controllers/api/v1/rider_logins_controller.rb
@@ -1,10 +1,33 @@
 class Api::V1::RiderLoginsController < ApplicationController
-  def index
-  end
+  before_action : authorise_access_request!, only:[:destroy]
 
-  def show
-  end
+    def create
+      rider =Rider.find_by(email: params[:email])
+      if rider.authenticate(params[:password])
+        payload ={rider_id: rider.id}
+        session = JWTSession::Session.new(payload: payload, refresh_by_access_allowed: true )
+        token = session.login
+        response.set_cookie(JWTSession.access_cookie,
+                            value: token[access],
+                            httponly: true,
+                            secure: Rails.env.production?
+                          )
 
-  def update
+        render json: {csrf: tokens [:csrf]}
+      else
+        unauthorised
+      end                    
+    end
+     
+    def destroy 
+      session = JWTSession::Session.new(payload: payload)
+      session.flush_by_access_payload
+      render json: :ok
+    end
+
+    private
+
+    def not_available
+    render json: {error: "Email/Password combination is incorrect"}, status: :not_available
+    end
   end
-end
